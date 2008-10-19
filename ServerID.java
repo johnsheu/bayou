fa077c3c34d15f16f9ae463f97d43e1ef75131bb
John Sheu <john.sheu@gmail.com>
2008-10-19 06:43:43 +0000

    Cleanup, and a bugfix on ServerID
    git-svn-id: file:///home/sheu/svn/bayou@11 f546f330-0b42-417b-95bf-8a63a2b938e6

--- ServerID.java
+++ ServerID.java
@@ -1,45 +1,47 @@
 import java.math.BigInteger;
 
-public class ServerID implements Comparable
+public class ServerID implements Comparable<ServerID>
 {
+	private ServerID sid;
+	private BigInteger acceptStamp;
 
-    private ServerID sid;
-    private BigInteger acceptStamp;
-    private boolean isFirst;
+	public ServerID( ServerID sid, BigInteger acceptStamp )
+	{
+		this.sid = sid;
+		this.acceptStamp = acceptStamp;
+	}
 
-    public ServerID( ServerID sid, BigInteger acceptStamp )
-    {
-	this.sid = sid;
-	this.acceptStamp = acceptStamp;
-    }
+	public boolean equals( ServerID otherServer )
+	{
+		if ( otherServer == null )
+			return false;
+		if ( sid == otherServer.sid &&
+			acceptStamp.equals( otherServer.acceptStamp ) )
+			return true;
+		return false;
+	}
 
-    public boolean equals(ServerID otherServer)
-    {
-	if( otherServer == null )
-	    return false;
-	if( sid == otherServer.sid && acceptStamp.equals(otherServer.acceptStamp) )
-	    return true;
+	public int compareTo( ServerID otherID )
+	{
+		if ( this == otherID )
+			return 0;
 
-	return false;
-    }
+		//  Can't forget the case when both SIDs are null
+		if ( this.sid == null )
+		{
+			if ( otherID.sid == null )
+				return 0;
+			else
+				return -1;
+		}
+		else if ( otherID.sid == null )
+			return 1;
 
-    public int compareTo( Object otherObject )
-    {
-	ServerID otherID = (ServerID) otherObject;
-
-	if( this == otherID )
-	    return 0;
-	else if( this.sid == null && otherID.sid != null )
-	    return -1;
-	else if( this.sid != null && otherID.sid == null)
-	    return 1;
-
-	int result = otherID.sid.compareTo(this.sid);
-	if( result == 0 )
-	    return this.acceptStamp.compareTo(otherID.acceptStamp);
-	else if( result < 0 )
-	    return -1;
-	else
-	    return 1;
-    }
+		int result = otherID.sid.compareTo( this.sid );
+		if ( result == 0 )
+			return this.acceptStamp.compareTo( otherID.acceptStamp );
+		else
+			return result;
+	}
 }
+
