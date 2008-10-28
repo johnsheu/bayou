98e413e121f147a4ba1b7659aa8978506d619bff
Karen Milfeld <kmilfeld@gmail.com>
2008-10-28 02:36:42 +0000

    Added in support for CSN.
    git-svn-id: file:///home/sheu/svn/bayou@33 f546f330-0b42-417b-95bf-8a63a2b938e6

--- BayouAERequest.java
+++ BayouAERequest.java
@@ -10,13 +10,16 @@ public class BayouAERequest extends Message implements Serializable
 
 	private long recvOSN = -1;
 
+	private long recvCSN = -1;
+
 	private HashMap<ServerID, Long> recvVV = null;
 
-	public BayouAERequest( ServerID ID, long OSN,
+	public BayouAERequest( ServerID ID, long OSN, long CSN,
 		HashMap<ServerID, Long> VV )
 	{
 		recvID = ID;
 		recvOSN = OSN;
+		recvCSN = CSN;
 		recvVV = VV;
 	}
 
@@ -28,6 +31,11 @@ public class BayouAERequest extends Message implements Serializable
 	public long getRecvOSN()
 	{
 		return recvOSN;
+	}
+
+	public long getRecvCSN()
+	{
+		return recvCSN;
 	}
 
 	public HashMap<ServerID, Long> getRecvVV()
